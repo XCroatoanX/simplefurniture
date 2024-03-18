5e624320670112fe79f53c4f502958e69adeb2f8
XCroatoanX <stasyukartem2@gmail.com>
2024-03-18 16:45:35 +0100

    Finished header.component

---
+++ frontend/src/app/core/header/header.component.ts
@@ -0,0 +1,19 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import "@material/web/textfield/filled-text-field";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatInputModule, RouterModule, MatButtonModule]
+})
+export class HeaderComponent {
+
+}
